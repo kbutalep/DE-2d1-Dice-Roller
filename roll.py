7662500df633c4380db33e6df4be658c219932cf
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-15 09:32:14 -0400

    finished lab, added roll again option

--- roll.py
+++ roll.py
@@ -7,15 +7,25 @@
 
 import random
 
-die_to_roll = int(input("How many dice do you want to roll? 1, 2, or 3?"))
+def roll_dice():
+    die_to_roll = int(input("How many dice do you want to roll? 1, 2, or 3?"))
 
-if die_to_roll == 1:
-    print(random.randint(1,6))
-elif die_to_roll == 2:
-    print(random.randint(1,6))
-    print(random.randint(1, 6))
-elif die_to_roll == 3:
-    print(random.randint(1,6))
-    print(random.randint(1, 6))
-    print(random.randint(1, 6))
+    if die_to_roll == 1:
+        print(random.randint(1,6))
+    elif die_to_roll == 2:
+        print(random.randint(1,6))
+        print(random.randint(1, 6))
+    elif die_to_roll == 3:
+        print(random.randint(1,6))
+        print(random.randint(1, 6))
+        print(random.randint(1, 6))
+
+    print("Do you want to roll again? Select 1 for Yes, 2 for No\n")
+    answer = int(input())
+    if answer == 1:
+        roll_dice()
+    else:
+        print("Goodbye!")
+
+roll_dice()
 
